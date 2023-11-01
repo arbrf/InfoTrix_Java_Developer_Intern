36bc0ed500903bdb388e08febac4de27a93643e9
Harry <shaikmohammadibrahim311@gmail.com>
2023-11-01 19:01:52 +0530

    Structure

---
+++ Currency-Converter/src/main/java/com/Infotrix/week1/Task1/Currency/Converter/CurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.Infotrix.week1.Task1.Currency.Converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+}
